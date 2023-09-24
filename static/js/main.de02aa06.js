8696cd6fad9a8eff6d79ba1d7627ab0a4e9b27d1
Nyurka <nyurka.mart@gmail.com>
2023-09-24 21:50:35 +0300

    initial commit

---
+++ static/js/main.de02aa06.js
@@ -0,0 +1,2 @@
+!function(){var r={277:function(){var r=window.$;r(document).on("ready",(function(){r(".carousel").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,autoplay:!1,prevArrow:r(".carousel-holder .btn-prev"),nextArrow:r(".carousel-holder .btn-next")})}))}},o={};function e(n){var t=o[n];if(void 0!==t)return t.exports;var i=o[n]={exports:{}};return r[n](i,i.exports,e),i.exports}!function(){"use strict";e(277)}()}();
+//# sourceMappingURL=main.de02aa06.js.map
